d6f292e02d9834028cc0c52fb4e7e96a7cf80384
crynobone <crynobone@gmail.com>
2015-09-21 18:42:20 +0800

    Simplify the class.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Creator/GitSubmoduleCreator.php
+++ src/Creator/GitSubmoduleCreator.php
@@ -2,34 +2,10 @@
 
 namespace Studio\Creator;
 
-use Studio\Package;
 use Studio\Shell\Shell;
 
-class GitSubmoduleCreator implements CreatorInterface
+class GitSubmoduleCreator extends GitRepoCreator
 {
-
-    protected $repo;
-
-    protected $path;
-
-
-    public function __construct($repo, $path)
-    {
-        $this->repo = $repo;
-        $this->path = $path;
-    }
-
-    /**
-     * Create the new package.
-     *
-     * @return \Studio\Package
-     */
-    public function create()
-    {
-        $this->cloneRepository();
-
-        return Package::fromFolder($this->path);
-    }
 
     protected function cloneRepository()
     {
