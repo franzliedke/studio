710808a1ff53fd06ef40bf31063fe4d37bdcab5f
Franz Liedke <franz@develophp.org>
2015-12-12 23:06:26 +0100

    Remove unused imports

--- src/Console/LoadCommand.php
+++ src/Console/LoadCommand.php
@@ -8,9 +8,7 @@ use Studio\Shell\Shell;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 
 class LoadCommand extends Command
 {
--- src/Console/ScrapCommand.php
+++ src/Console/ScrapCommand.php
@@ -8,9 +8,7 @@ use Studio\Shell\Shell;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
 
 class ScrapCommand extends Command
