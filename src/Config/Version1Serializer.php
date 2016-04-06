549578a7f28d2cf2379705dc6649964f528b9ec9
Franz Liedke <franz@develophp.org>
2016-04-06 22:01:09 +0900

    Backwards-compatible implementation of the Version1Serializer

--- src/Config/Version1Serializer.php
+++ src/Config/Version1Serializer.php
@@ -2,6 +2,8 @@
 
 namespace Studio\Config;
 
+use Studio\Package;
+
 class Version1Serializer implements Serializer
 {
     public function deserializePaths($obj)
@@ -11,6 +13,32 @@ class Version1Serializer implements Serializer
 
     public function serializePaths(array $paths)
     {
-        return ['packages' => $paths];
+        $globbedPaths = array_map(function ($path) {
+            return glob($path, GLOB_MARK | GLOB_ONLYDIR);
+        }, $paths);
+
+        $allPaths = array_reduce($globbedPaths, function ($collect, $pathOrPaths) {
+            if (is_array($pathOrPaths)) {
+                return array_merge($collect, $pathOrPaths);
+            } else {
+                $collect[] = $pathOrPaths;
+                return $collect;
+            }
+        }, []);
+
+        $allPaths = array_filter($allPaths, function ($path) {
+            return is_dir($path) && file_exists("$path/composer.json");
+        });
+
+        $packages = array_map(function ($path) {
+            return Package::fromFolder(rtrim($path, '/'));
+        }, $allPaths);
+
+        $packagePaths = array_reduce($packages, function ($collect, Package $package) {
+            $collect[$package->getComposerId()] = $package->getPath();
+            return $collect;
+        }, []);
+
+        return ['packages' => $packagePaths];
     }
 }
