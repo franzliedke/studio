e89657fc1483ce338864eac641dabbdb5f9d269e
Franz Liedke <franz@develophp.org>
2016-04-06 21:20:46 +0900

    Implement a VersionedSerializer class to support reading/writing different config versions
    In preparation for #54.

---
+++ src/Config/VersionedSerializer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Studio\Config;
+
+class VersionedSerializer implements Serializer
+{
+    /**
+     * @var Serializer[]
+     */
+    protected $serializers;
+
+    /**
+     * @var int
+     */
+    protected $defaultVersion;
+
+    /**
+     * @param int $version
+     * @param Serializer $serializer
+     * @return static
+     */
+    public static function withDefault($version, Serializer $serializer)
+    {
+        return new static([$version => $serializer], $version);
+    }
+
+    public function __construct(array $serializers, $defaultVersion)
+    {
+        $this->serializers = $serializers;
+        $this->defaultVersion = $defaultVersion;
+    }
+
+    public function version($version, Serializer $serializer)
+    {
+        $this->serializers[$version] = $serializer;
+
+        return $this;
+    }
+
+    public function deserializePaths($obj)
+    {
+        if (!isset($obj['version'])) {
+            $serializer = $this->serializers[$this->default];
+        } else if (array_key_exists(intval($obj['version']), $this->serializers)) {
+            $serializer = $this->serializers[$obj['version']];
+        } else {
+            throw new \InvalidArgumentException('Invalid version');
+        }
+
+        return $serializer->deserializePaths($obj);
+    }
+
+    public function serializePaths($paths)
+    {
+        $lastSerializer = $this->serializers[max(array_keys($this->serializers))];
+
+        return $lastSerializer->serializePaths($paths);
+    }
+}
