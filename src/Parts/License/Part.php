a740716f9bbc8f9288190537d1c69c629618578f
Franz Liedke <franz@develophp.org>
2016-04-11 21:24:33 +0900

    More work on license part
    Refs #11.

--- src/Parts/License/Part.php
+++ src/Parts/License/Part.php
@@ -13,9 +13,22 @@ class Part extends AbstractPart
         if ($this->input->confirm('Do you want to configure a license for your project?')) {
             $licenses = new SpdxLicenses();
 
-            // 1. Choose a license from a list
-            // 2. Generate the license file, with year and name
-            // 3. Add it to composer.json
+            $license = $this->selectLicenseFromList($licenses);
+
+            $this->copyLicenseFileTo($target, $license);
+
+            $composer->license = $license;
         }
+    }
+
+    protected function selectLicenseFromList($licenses)
+    {
+        // Ask the user to chosse a license from the list
+    }
+
+    protected function copyLicenseFileTo(Directory $target, $license)
+    {
+        // Download the file
+        // Add year and name
     }
 }
