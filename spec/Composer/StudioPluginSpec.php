b2c423736f2a2d3ceaf907b0edfe022118d87d79
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2020-02-06 16:44:18 +0100

    Add more tests for wildcard glob support and fix other tests

--- spec/Composer/StudioPluginSpec.php
+++ spec/Composer/StudioPluginSpec.php
@@ -4,6 +4,9 @@ namespace spec\Studio\Composer;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Repository\ArrayRepository;
+use Composer\Repository\RepositoryManager;
+use Composer\Repository\WritableArrayRepository;
 use Composer\Script\ScriptEvents;
 use PhpSpec\ObjectBehavior;
 
@@ -14,8 +17,14 @@ class StudioPluginSpec extends ObjectBehavior
         $this->shouldHaveType('Studio\Composer\StudioPlugin');
     }
 
-    function it_is_activatable(Composer $composer, IOInterface $io)
+    function it_is_activatable(Composer $composer, IOInterface $io, RepositoryManager $repositoryManager)
     {
+        // Mock methods
+        $composer->getInstallationManager()->willReturn(null);
+        $composer->getDownloadManager()->willReturn(null);
+        $composer->getPackage()->willReturn(null);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
+
         $this->activate($composer, $io);
     }
 
@@ -32,7 +41,7 @@ class StudioPluginSpec extends ObjectBehavior
     /**
      * Test if studio does not create symlinks when no studio.json is defined
      */
-    function it_doesnt_create_symlinks_without_file($composer, $io, $rootPackage, $filesystem)
+    function it_doesnt_create_symlinks_without_file($composer, $io, $rootPackage, $filesystem, $repositoryManager)
     {
         // switch working directory
         chdir(__DIR__);
@@ -42,6 +51,7 @@ class StudioPluginSpec extends ObjectBehavior
         $rootPackage->beADoubleOf('Composer\Package\RootPackage');
         $composer->beADoubleOf('Composer\Composer');
         $io->beADoubleOf('Composer\IO\IOInterface');
+        $repositoryManager->beADoubleOf('Composer\Repository\RepositoryManager');
 
         // Construct
         $this->beConstructedWith($filesystem);
@@ -50,6 +60,7 @@ class StudioPluginSpec extends ObjectBehavior
         $composer->getInstallationManager()->willReturn(null);
         $composer->getDownloadManager()->willReturn(null);
         $composer->getPackage()->willReturn($rootPackage);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
         $rootPackage->getTargetDir()->willReturn(getcwd());
 
         // Test
@@ -60,7 +71,7 @@ class StudioPluginSpec extends ObjectBehavior
     /**
      * Test if studio does not unlink when no studio.json or .studio/studio.json is defined
      */
-    function it_doesnt_unlink_without_files($composer, $io, $rootPackage, $filesystem)
+    function it_doesnt_unlink_without_files($composer, $io, $rootPackage, $filesystem, $repositoryManager)
     {
         // switch working directory
         chdir(__DIR__);
@@ -70,6 +81,7 @@ class StudioPluginSpec extends ObjectBehavior
         $rootPackage->beADoubleOf('Composer\Package\RootPackage');
         $composer->beADoubleOf('Composer\Composer');
         $io->beADoubleOf('Composer\IO\IOInterface');
+        $repositoryManager->beADoubleOf('Composer\Repository\RepositoryManager');
 
         // Construct
         $this->beConstructedWith($filesystem);
@@ -78,6 +90,7 @@ class StudioPluginSpec extends ObjectBehavior
         $composer->getInstallationManager()->willReturn(null);
         $composer->getDownloadManager()->willReturn(null);
         $composer->getPackage()->willReturn($rootPackage);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
         $rootPackage->getTargetDir()->willReturn(getcwd());
 
         // Test
@@ -95,7 +108,11 @@ class StudioPluginSpec extends ObjectBehavior
         $filesystem,
         $installationManager,
         $downloadManager,
-        $pathDownloader
+        $pathDownloader,
+        $repositoryManager,
+        $localRepository,
+        $libraryPackage,
+        $library2Package
     ) {
         // switch working directory
         chdir(__DIR__ . '/stubs/project-with-path');
@@ -108,6 +125,16 @@ class StudioPluginSpec extends ObjectBehavior
         $installationManager->beADoubleOf('Composer\Installer\InstallationManager');
         $downloadManager->beADoubleOf('Composer\Downloader\DownloadManager');
         $pathDownloader->beADoubleOf('Composer\Downloader\PathDownloader');
+        $repositoryManager->beADoubleOf('Composer\Repository\RepositoryManager');
+        $localRepository->beADoubleOf('Composer\Repository\WritableRepositoryInterface');
+        $libraryPackage->beADoubleOf('Composer\Package\Package');
+        $library2Package->beADoubleOf('Composer\Package\Package');
+
+        $libraryPackage->getName()->willReturn("acme/library");
+        $library2Package->getName()->willReturn("acme/library2");
+
+        $localRepository->getPackages()->willReturn([$libraryPackage, $library2Package]);
+        $repositoryManager->getLocalRepository()->willReturn($localRepository);
 
         // Construct
         //$this->beConstructedWith($filesystem);
@@ -116,12 +143,15 @@ class StudioPluginSpec extends ObjectBehavior
         $composer->getInstallationManager()->willReturn($installationManager);
         $composer->getDownloadManager()->willReturn($downloadManager);
         $composer->getPackage()->willReturn($rootPackage);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
         $rootPackage->getTargetDir()->willReturn(getcwd());
         $downloadManager->getDownloader('path')
             ->willReturn($pathDownloader)
             ->shouldBeCalled();
 
-        $io->write('[Studio] Creating link to library for package acme/library')->shouldBeCalled();
+        $io->write('[Studio] Creating link to ../libs/library for package acme/library')->shouldBeCalled();
+        $io->write('[Studio] Creating link to ../libs/library2 for package acme/library2')->shouldNotBeCalled();
+        $io->write('[Studio] Creating link to ../libs/another-library for package acme/another-library')->shouldNotBeCalled();
 
         // Test
         $this->activate($composer, $io);
@@ -138,7 +168,8 @@ class StudioPluginSpec extends ObjectBehavior
         $rootPackage,
         $filesystem,
         $installationManager,
-        $pathDownloader
+        $pathDownloader,
+        $repositoryManager
     ) {
         // switch working directory
         chdir(__DIR__ . '/stubs/project-with-unload');
@@ -150,6 +181,7 @@ class StudioPluginSpec extends ObjectBehavior
         $io->beADoubleOf('Composer\IO\IOInterface');
         $installationManager->beADoubleOf('Composer\Installer\InstallationManager');
         $pathDownloader->beADoubleOf('Composer\Downloader\PathDownloader');
+        $repositoryManager->beADoubleOf('Composer\Repository\RepositoryManager');
 
         // Construct
         $this->beConstructedWith($filesystem);
@@ -158,11 +190,122 @@ class StudioPluginSpec extends ObjectBehavior
         $composer->getInstallationManager()->willReturn($installationManager);
         $composer->getDownloadManager()->willReturn(null);
         $composer->getPackage()->willReturn($rootPackage);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
         $rootPackage->getTargetDir()->willReturn(getcwd());
         $filesystem->isSymlinkedDirectory(null)->willReturn(true)->shouldBeCalled();
         $filesystem->removeDirectory(null)->shouldBeCalled();
 
-        $io->write('[Studio] Removing linked path library for package acme/library')->shouldBeCalled();
+        $io->write('[Studio] Removing linked path ../libs/library for package acme/library')->shouldBeCalled();
+        $io->write('[Studio] Removing linked path ../libs/library2 for package acme/library2')->shouldNotBeCalled();
+        $io->write('[Studio] Removing linked path ../libs/another-library for package acme/another-library')->shouldNotBeCalled();
+
+        // Test
+        $this->activate($composer, $io);
+        $this->unlinkStudioPackages();
+    }
+
+    /**
+     * Test if studio does create symlinks when studio.json is defined and contains wildcards path
+     */
+    function it_does_create_symlinks_with_file_and_wildcard_paths(
+        $composer,
+        $io,
+        $rootPackage,
+        $filesystem,
+        $installationManager,
+        $downloadManager,
+        $pathDownloader,
+        $repositoryManager,
+        $localRepository,
+        $libraryPackage,
+        $library2Package
+    ) {
+        // switch working directory
+        chdir(__DIR__ . '/stubs/project-with-path-wildcard');
+
+        // create stubs
+        $filesystem->beADoubleOf('Composer\Util\Filesystem');
+        $rootPackage->beADoubleOf('Composer\Package\RootPackage');
+        $composer->beADoubleOf('Composer\Composer');
+        $io->beADoubleOf('Composer\IO\IOInterface');
+        $installationManager->beADoubleOf('Composer\Installer\InstallationManager');
+        $downloadManager->beADoubleOf('Composer\Downloader\DownloadManager');
+        $pathDownloader->beADoubleOf('Composer\Downloader\PathDownloader');
+        $repositoryManager->beADoubleOf('Composer\Repository\RepositoryManager');
+        $localRepository->beADoubleOf('Composer\Repository\WritableRepositoryInterface');
+        $libraryPackage->beADoubleOf('Composer\Package\Package');
+        $library2Package->beADoubleOf('Composer\Package\Package');
+
+        $libraryPackage->getName()->willReturn("acme/library");
+        $library2Package->getName()->willReturn("acme/library2");
+
+        $localRepository->getPackages()->willReturn([$libraryPackage, $library2Package]);
+        $repositoryManager->getLocalRepository()->willReturn($localRepository);
+
+        $repositoryManager->getLocalRepository()->willReturn($localRepository);
+
+        // Construct
+        //$this->beConstructedWith($filesystem);
+
+        // Mock methods
+        $composer->getInstallationManager()->willReturn($installationManager);
+        $composer->getDownloadManager()->willReturn($downloadManager);
+        $composer->getPackage()->willReturn($rootPackage);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
+        $rootPackage->getTargetDir()->willReturn(getcwd());
+        $downloadManager->getDownloader('path')
+            ->willReturn($pathDownloader)
+            ->shouldBeCalled();
+
+        $io->write('[Studio] Creating link to ../libs/library for package acme/library')->shouldBeCalled();
+        $io->write('[Studio] Creating link to ../libs/library2 for package acme/library2')->shouldBeCalled();
+        $io->write('[Studio] Creating link to ../libs/another-library for package acme/another-library')->shouldNotBeCalled();
+
+        // Test
+        $this->activate($composer, $io);
+        $this->symlinkStudioPackages();
+    }
+
+
+    /**
+     * Test if studio does unlink when studio.json is defined and contains wildcard paths.
+     */
+    function it_does_unlink_with_file_and_wildcard_paths(
+        $composer,
+        $io,
+        $rootPackage,
+        $filesystem,
+        $installationManager,
+        $pathDownloader,
+        $repositoryManager
+    ) {
+        // switch working directory
+        chdir(__DIR__ . '/stubs/project-with-unload-wildcard');
+
+        // create stubs
+        $filesystem->beADoubleOf('Composer\Util\Filesystem');
+        $rootPackage->beADoubleOf('Composer\Package\RootPackage');
+        $composer->beADoubleOf('Composer\Composer');
+        $io->beADoubleOf('Composer\IO\IOInterface');
+        $installationManager->beADoubleOf('Composer\Installer\InstallationManager');
+        $pathDownloader->beADoubleOf('Composer\Downloader\PathDownloader');
+        $repositoryManager->beADoubleOf('Composer\Repository\RepositoryManager');
+
+        // Construct
+        $this->beConstructedWith($filesystem);
+
+        // Mock methods
+        $composer->getInstallationManager()->willReturn($installationManager);
+        $composer->getDownloadManager()->willReturn(null);
+        $composer->getPackage()->willReturn($rootPackage);
+        $composer->getRepositoryManager()->willReturn($repositoryManager);
+        $rootPackage->getTargetDir()->willReturn(getcwd());
+        $filesystem->isSymlinkedDirectory(null)->willReturn(true)->shouldBeCalled();
+        $filesystem->removeDirectory(null)->shouldBeCalled();
+
+        $io->write('[Studio] Removing linked path ../libs/library for package acme/library')->shouldBeCalled();
+        $io->write('[Studio] Removing linked path ../libs/library2 for package acme/library2')->shouldBeCalled();
+        $io->write('[Studio] Removing linked path ../libs/another-library for package acme/another-library')->shouldBeCalled();
 
         // Test
         $this->activate($composer, $io);
